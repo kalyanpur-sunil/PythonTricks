0faa4445e8df1ecd22320b5ea4a1cd1752ac5889
Sunil Kalyanpur <skalyanpur@Sunils-MacBook-Pro.local>
2015-07-19 16:26:26 -0700

    more changes

--- python-tricks.py
+++ python-tricks.py
@@ -1,8 +1,9 @@
 from __future__ import division
 from pprint import pprint
 import pdb
-
-
+import collections
+ 
+from collections import defaultdict
 # enumerate
 list_a = ['horse', 'cat', 'elephant']
 for i, item in enumerate(list_a):
@@ -101,4 +102,87 @@ pprint(my_dictionary)
  # 8: 16777216,
  # 9: 387420489,
  # 10: 10000000000}
+
+
+# use of namedtuple
+india = ('Delhi', 'India', 'Asia')
+print india
+print '%s is capital of %s and is in %s' % india
+# it would be tough if there are large amount values.
+# namedtuple from colletions comes to our rescue
+
+Country = collections.namedtuple('Country', 'capital country continent')
+print 'Country type: ', type(Country)
+
+india = Country(capital='Delhi', country='India', continent='Asia')
+print india.country
+
+# How can I merge two Python dictionaries in a single expression?
+x = {'a': 1, 'b': 2}
+y = {'b': 3, 'c': 4}
+a = {'f': 6}
+# it is a two step process
+# since we don't want to alter x, make a copy of x and assign to z.
+# update x with y
+
+z = x.copy()
+z.update(y)
+
+print 'Updated z: ', z
+
+result = {}
+def merge_dicts(*dicts):
+	for dictionary in dicts:
+		result.update(dictionary)
+
+	return result
+
+print 'calling merge_dicts() : ', merge_dicts(x,y,a)	
+
+lst1 = [{"id": 1, "x": "one"}, {"id": 2, "x": "two"}]
+lst2 = [{"id": 2, "x": "two"}, {"id": 3, "x": "three"}]
+
+tmp_list = []
+# for d in lst1:
+# 	tmp_list.append(d)
+# for d in lst2:
+# 	tmp_list.append(d)
+tmp_list.extend(lst1)
+tmp_list.extend(lst2)
+
+print 'temp list', tmp_list
+
+
+# defaultdict
+# a defaultdict is a dictonary with a default value for keys
+key_list = ['ca','il','ia', 'il']
+# not a good way of doing
+f={}
+for key in key_list:
+	try:
+		f[key] += 1
+	except KeyError, e:
+		f[key] = 1
+
+print f
+
+
+# good way of doing
+frequency = defaultdict(int)
+
+for key in key_list:
+	frequency[key] += 1
+
+print "frequency defaultdict: ", frequency
+
+temp_dict = {'a':3, 'v':4}
+
+# check if key exists in the dictionary
+key='a'
+if key in temp_dict:
+	print 'key present'
+
+
+
+
 
